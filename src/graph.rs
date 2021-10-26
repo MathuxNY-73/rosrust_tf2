371cf8a61edfc98243e4e7eeababacfdffa76c9d
Antoine Pouillaude <a.pouillaude@nomagic.ai>
2021-10-26 10:10:24 +0200

    Refactors the code into several modules

---
+++ src/graph.rs
@@ -0,0 +1,13 @@
+#[derive(Clone,Debug,Hash)] 
+pub struct TfGraphNode {
+    pub child: String,
+    pub parent: String
+}
+
+impl PartialEq for TfGraphNode {
+    fn eq(&self, other: &Self) -> bool {
+        self.child == other.child && self.parent == other.parent
+    }
+}
+
+impl Eq for TfGraphNode {}
