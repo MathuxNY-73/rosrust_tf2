a66d483ee1388e65d3dd43d9351790993144dc00
Antoine Pouillaude <a.pouillaude@nomagic.ai>
2021-11-29 23:08:15 +0100

    Remove duplicate of transform objects

--- src/transforms.rs
+++ src/transforms.rs
@@ -5,36 +5,16 @@ use ndarray_linalg::trace::Trace;
 use ndarray_linalg::solve::Inverse;
 use nalgebra::geometry;
 
+use crate::msg;
 
-#[derive(PartialEq, Debug, Clone, Copy)]
-pub struct Quaternion {
-    pub x: f64,
-    pub y: f64,
-    pub z: f64,
-    pub w: f64
-}
-
-
-#[derive(PartialEq, Debug, Clone, Copy)]
-pub struct Position {
-    pub x: f64,
-    pub y: f64,
-    pub z: f64
-}
-
-#[derive(PartialEq, Debug, Clone, Copy)]
-pub struct Transform {
-    pub orientation: Quaternion,
-    pub position: Position
-}
 
 /// Converts a SE3 transformation matrix to a quaternion
-pub fn quaternion_from_transform_matrix(a: &Array2<f64>) ->  Quaternion {
+pub fn quaternion_from_transform_matrix(a: Array2<f64>) ->  msg::Quaternion {
     let tr = a.trace().unwrap();
     let trace = tr - 1f64;
     if trace > 0f64 {
         let qw = (1f64 + trace).sqrt()/2f64;
-        Quaternion{
+        msg::Quaternion{
             x: (a[[2,1]] - a[[1,2]])/(4f64*qw),
             y: (a[[0,2]] - a[[2,0]])/(4f64*qw), 
             z: (a[[1,0]] - a[[0,1]])/(4f64*qw), 
@@ -43,7 +23,7 @@ pub fn quaternion_from_transform_matrix(a: &Array2<f64>) ->  Quaternion {
     }
     else if a[[0,0]] > a[[1,1]] && a[[0,0]] > a[[2,2]] {
         let s = 2.0f64 * (1.0f64 + a[[0,0]] - a[[1,1]] - a[[2,2]]).sqrt();
-        Quaternion{
+        msg::Quaternion{
             w: (a[[2,1]] - a[[1,2]] ) / s,
             x: 0.25f64 * s,
             y: (a[[0,1]] + a[[1,0]] ) / s,
@@ -51,7 +31,7 @@ pub fn quaternion_from_transform_matrix(a: &Array2<f64>) ->  Quaternion {
         }
     } else if a[[1,1]] > a[[2,2]] {
         let s = 2.0f64 * (1.0f64 + a[[1,1]] - a[[0,0]] - a[[2,2]]).sqrt();
-        Quaternion{
+        msg::Quaternion{
             w: (a[[0,2]] - a[[2,0]] ) / s,
             x: (a[[0,1]] + a[[1,0]] ) / s,
             y: 0.25f64 * s,
@@ -59,7 +39,7 @@ pub fn quaternion_from_transform_matrix(a: &Array2<f64>) ->  Quaternion {
         }
     } else {
         let s = 2.0f64 * (1.0f64 + a[[2,2]] - a[[0,0]] - a[[1,1]]).sqrt();
-        Quaternion{
+        msg::Quaternion{
             w: (a[[1,0]] - a[[0,1]] ) / s,
             x: (a[[0,2]] + a[[2,0]] ) / s,
             y: (a[[1,2]] + a[[2,1]] ) / s,
@@ -69,7 +49,7 @@ pub fn quaternion_from_transform_matrix(a: &Array2<f64>) ->  Quaternion {
 }
 
 /// Converts a quaternion to an SE3 matrix
-pub fn transform_matrix_from_quaternion(msg: &Quaternion) ->  Array2<f64> {
+pub fn transform_matrix_from_quaternion(msg: msg::Quaternion) ->  Array2<f64> {
     let s  = (msg.x*msg.x+ msg.y*msg.y + msg.z*msg.z + msg.w*msg.w).sqrt();
     let a =  arr2(&[[1f64 - 2f64*s*(msg.y*msg.y + msg.z*msg.z), 2f64*s*(msg.x*msg.y - msg.z*msg.w), 2f64*s*(msg.x*msg.z + msg.y*msg.w), 0f64],
                     [2f64*s*(msg.x*msg.y - msg.w*msg.z), 1f64 - 2f64*s*(msg.x*msg.x + msg.z*msg.z), 2f64*s*(msg.y*msg.z + msg.w*msg.x), 0f64],
@@ -79,58 +59,58 @@ pub fn transform_matrix_from_quaternion(msg: &Quaternion) ->  Array2<f64> {
 }
 
 ///Converts a transform from xyz translation + quaternion format to an SE3 matrix
-pub fn se3_from_transform(transform: &Transform) -> Array2<f64> {
-    let mut a = transform_matrix_from_quaternion(&transform.orientation);
-    a[[3,0]] = transform.position.x;
-    a[[3,1]] = transform.position.y;
-    a[[3,2]] = transform.position.z;
+pub fn se3_from_transform(transform: msg::Transform) -> Array2<f64> {
+    let mut a = transform_matrix_from_quaternion(transform.rotation);
+    a[[3,0]] = transform.translation.x;
+    a[[3,1]] = transform.translation.y;
+    a[[3,2]] = transform.translation.z;
     a
 }
 
 ///Converts an SE3 matrix to a Transform
-pub fn transform_from_se3(a: &Array2<f64>) -> Transform {
-    Transform {
-        position: Position {
+pub fn transform_from_se3(a: Array2<f64>) -> msg::Transform {
+    msg::Transform {
+        translation: msg::Vector3 {
             x: a[[3,0]],
             y: a[[3,1]],
             z: a[[3,2]]
         },
-        orientation: quaternion_from_transform_matrix(a)
+        rotation: quaternion_from_transform_matrix(a)
     }
 }
 
 ///Get the inverse transform
-pub fn invert_transform(transform: &Transform) -> Transform {
-    let m = se3_from_transform(&transform);
+pub fn invert_transform(transform: msg::Transform) -> msg::Transform {
+    let m = se3_from_transform(transform);
     let m_inv = m.inv().unwrap();
-    transform_from_se3(&m_inv)
+    transform_from_se3(m_inv)
 }
 
 ///Chain multiple transforms together. Takes in a vector of transforms. The vector should be in order of desired transformations
-pub fn chain_transforms(transforms: &Vec<Transform>) -> Transform {    
+pub fn chain_transforms(transforms: Vec<msg::Transform>) -> msg::Transform {
     let mut final_transform = Array::eye(4);
     for t in transforms {
-        let tf = se3_from_transform(&t);
+        let tf = se3_from_transform(t);
         final_transform = tf.dot(&final_transform);
     } 
-    transform_from_se3(&final_transform)
+    transform_from_se3(final_transform)
 }
 
-pub fn interpolate(t1: Transform, t2: Transform, weight: f64) -> Transform {
-    let r1 = geometry::Quaternion::new(t1.orientation.w, t1.orientation.x, t1.orientation.y, t1.orientation.z);
-    let r2 = geometry::Quaternion::new(t2.orientation.w, t2.orientation.x, t2.orientation.y, t2.orientation.z);
+pub fn interpolate(t1: msg::Transform, t2: msg::Transform, weight: f64) -> msg::Transform {
+    let r1 = geometry::Quaternion::new(t1.rotation.w, t1.rotation.x, t1.rotation.y, t1.rotation.z);
+    let r2 = geometry::Quaternion::new(t2.rotation.w, t2.rotation.x, t2.rotation.y, t2.rotation.z);
     let r1 = geometry::UnitQuaternion::from_quaternion(r1);
     let r2 = geometry::UnitQuaternion::from_quaternion(r2);
     let res  = r1.try_slerp(&r2, weight, 1e-9);
     match res {
         Some(qt) => {
-            Transform{
-                position: Position {
-                    x: t1.position.x * weight + t2.position.x * (1.0 - weight),
-                    y: t1.position.y * weight + t2.position.y * (1.0 - weight),
-                    z: t1.position.z * weight + t2.position.z * (1.0 - weight) 
+            msg::Transform{
+                translation: msg::Vector3 {
+                    x: t1.translation.x * weight + t2.translation.x * (1.0 - weight),
+                    y: t1.translation.y * weight + t2.translation.y * (1.0 - weight),
+                    z: t1.translation.z * weight + t2.translation.z * (1.0 - weight) 
                 },
-                orientation: Quaternion {
+                rotation: msg::Quaternion {
                     x: qt.coords[0],
                     y: qt.coords[1],
                     z: qt.coords[2],
@@ -140,23 +120,23 @@ pub fn interpolate(t1: Transform, t2: Transform, weight: f64) -> Transform {
         }
         None => {
             if weight > 0.5 {
-                Transform{
-                    position: Position {
-                        x: t1.position.x * weight + t2.position.x * (1.0 - weight),
-                        y: t1.position.y * weight + t2.position.y * (1.0 - weight),
-                        z: t1.position.z * weight + t2.position.z * (1.0 - weight) 
+                msg::Transform{
+                    translation: msg::Vector3 {
+                        x: t1.translation.x * weight + t2.translation.x * (1.0 - weight),
+                        y: t1.translation.y * weight + t2.translation.y * (1.0 - weight),
+                        z: t1.translation.z * weight + t2.translation.z * (1.0 - weight) 
                     },
-                    orientation: t1.orientation.clone()
+                    rotation: t1.rotation.clone()
                 }
             }
             else {
-                Transform{
-                    position: Position {
-                        x: t1.position.x * weight + t2.position.x * (1.0 - weight),
-                        y: t1.position.y * weight + t2.position.y * (1.0 - weight),
-                        z: t1.position.z * weight + t2.position.z * (1.0 - weight) 
+                msg::Transform{
+                    translation: msg::Vector3 {
+                        x: t1.translation.x * weight + t2.translation.x * (1.0 - weight),
+                        y: t1.translation.y * weight + t2.translation.y * (1.0 - weight),
+                        z: t1.translation.z * weight + t2.translation.z * (1.0 - weight) 
                     },
-                    orientation: t2.orientation.clone()
+                    rotation: t2.rotation.clone()
                 }
             }
         } 
@@ -169,84 +149,84 @@ mod test {
 
     #[test]
     fn test_quaternion_identity_basic1(){
-        let qt = Quaternion{
+        let qt = msg::Quaternion {
             x: 0f64,
             y: 0f64,
             z: 0f64,
             w: 1f64
         };
-        let arr = transform_matrix_from_quaternion(&qt);
-        let q2 = quaternion_from_transform_matrix(&arr);
+        let arr = transform_matrix_from_quaternion(qt.clone());
+        let q2 = quaternion_from_transform_matrix(arr);
         assert_eq!(qt, q2);
     }
 
     #[test]
     fn test_quaternion_identity_basic2(){
-        let qt = Quaternion{
+        let qt = msg::Quaternion{
             x: 0f64,
             y: 0f64,
             z: 1f64,
             w: 0f64
         };
-        let arr = transform_matrix_from_quaternion(&qt);
-        let q2 = quaternion_from_transform_matrix(&arr);
+        let arr = transform_matrix_from_quaternion(qt.clone());
+        let q2 = quaternion_from_transform_matrix(arr);
         assert_eq!(qt, q2);
     }
 
     #[test]
     fn test_quaternion_identity_basic3(){
-        let qt = Quaternion{
+        let qt = msg::Quaternion{
             x: 0f64,
             y: 1f64,
             z: 0f64,
             w: 0f64
         };
-        let arr = transform_matrix_from_quaternion(&qt);
-        let q2 = quaternion_from_transform_matrix(&arr);
+        let arr = transform_matrix_from_quaternion(qt.clone());
+        let q2 = quaternion_from_transform_matrix(arr);
         assert_eq!(qt, q2);
     }
 
     #[test]
     fn test_quaternion_identity_basic4(){
-        let qt = Quaternion{
+        let qt = msg::Quaternion{
             x: 1f64,
             y: 0f64,
             z: 0f64,
             w: 0f64
         };
-        let arr = transform_matrix_from_quaternion(&qt);
-        let q2 = quaternion_from_transform_matrix(&arr);
+        let arr = transform_matrix_from_quaternion(qt.clone());
+        let q2 = quaternion_from_transform_matrix(arr);
         assert_eq!(qt, q2);
     }
 
     #[test]
     fn test_basic_translation_chaining(){
-        let tf1 = Transform {
-            position: Position{x: 1f64, y: 1f64, z: 0f64},
-            orientation: Quaternion{x: 0f64, y: 0f64, z: 0f64, w: 1f64}
+        let tf1 = msg::Transform {
+            translation: msg::Vector3{x: 1f64, y: 1f64, z: 0f64},
+            rotation: msg::Quaternion{x: 0f64, y: 0f64, z: 0f64, w: 1f64}
         };
-        let expected_tf = Transform {
-            position: Position{x: 2f64, y: 2f64, z: 0f64},
-            orientation: Quaternion{x: 0f64, y: 0f64, z: 0f64, w: 1f64}
+        let expected_tf = msg::Transform {
+            translation: msg::Vector3{x: 2f64, y: 2f64, z: 0f64},
+            rotation: msg::Quaternion{x: 0f64, y: 0f64, z: 0f64, w: 1f64}
         };
-        let transform_chain = vec!(tf1, tf1);
-        let res = chain_transforms(&transform_chain);
+        let transform_chain = vec!(tf1.clone(), tf1);
+        let res = chain_transforms(transform_chain);
         assert_eq!(res, expected_tf);
     }
 
     #[test]
     fn test_basic_interpolation() {
-        let tf1 = Transform {
-            position: Position{x: 1f64, y: 1f64, z: 0f64},
-            orientation: Quaternion{x: 0f64, y: 0f64, z: 0f64, w: 1f64}
+        let tf1 = msg::Transform {
+            translation: msg::Vector3{x: 1f64, y: 1f64, z: 0f64},
+            rotation: msg::Quaternion{x: 0f64, y: 0f64, z: 0f64, w: 1f64}
         };
-        let tf2 = Transform {
-            position: Position{x: 2f64, y: 2f64, z: 0f64},
-            orientation: Quaternion{x: 0f64, y: 0f64, z: 0f64, w: 1f64}
+        let tf2 = msg::Transform {
+            translation: msg::Vector3{x: 2f64, y: 2f64, z: 0f64},
+            rotation: msg::Quaternion{x: 0f64, y: 0f64, z: 0f64, w: 1f64}
         };
-        let expected = Transform {
-            position: Position{x: 1.5f64, y: 1.5f64, z: 0f64},
-            orientation: Quaternion{x: 0f64, y: 0f64, z: 0f64, w: 1f64}
+        let expected = msg::Transform {
+            translation: msg::Vector3{x: 1.5f64, y: 1.5f64, z: 0f64},
+            rotation: msg::Quaternion{x: 0f64, y: 0f64, z: 0f64, w: 1f64}
         };
         assert_eq!(interpolate(tf1, tf2, 0.5), expected);
     }
--- src/utils.rs
+++ src/utils.rs
@@ -5,22 +5,22 @@ use crate::msg;
 /// Calculates the inverse of a ros transform
 pub fn get_inverse(transform: msg::TransformStamped) -> msg::TransformStamped {
     
-    let m_transform = to_transform(transform.clone());
-    let inverse = transforms::invert_transform(&m_transform);
+    //let m_transform = to_transform(transform);
+    let inverse = transforms::invert_transform(transform.transform);
 
     let inv = msg::TransformStamped {
-        child_frame_id: transform.header.frame_id.clone(),
+        child_frame_id: transform.header.frame_id,
         header: msg::Header {
-            frame_id: transform.child_frame_id.clone(),
+            frame_id: transform.child_frame_id,
             stamp: transform.header.stamp,
             seq: transform.header.seq
         },
         transform: msg::Transform{
             rotation: msg::Quaternion{
-                x: inverse.orientation.x, y:inverse.orientation.y, z: inverse.orientation.z, w: inverse.orientation.w
+                x: inverse.rotation.x, y:inverse.rotation.y, z: inverse.rotation.z, w: inverse.rotation.w
             },
             translation: msg::Vector3{
-                x: inverse.position.x, y: inverse.position.y, z: inverse.position.z
+                x: inverse.translation.x, y: inverse.translation.y, z: inverse.translation.z
             }
         }
     };
@@ -28,39 +28,15 @@ pub fn get_inverse(transform: msg::TransformStamped) -> msg::TransformStamped {
 }
 
 
-pub fn to_transform(transform: msg::TransformStamped) -> transforms::Transform {
-    transforms::Transform {
-        orientation: transforms::Quaternion{
-            x: transform.transform.rotation.x,
-            y: transform.transform.rotation.y,
-            z: transform.transform.rotation.z,
-            w: transform.transform.rotation.w,
-        },
-        position: transforms::Position{
-            x: transform.transform.translation.x,
-            y: transform.transform.translation.y,
-            z: transform.transform.translation.z
-        }
-    }
-}
-
-
-pub fn to_transform_stamped(transform: transforms::Transform, from: std::string::String, to: std::string::String, time: rosrust::Time) -> msg::TransformStamped {
+pub fn to_transform_stamped(transform: msg::Transform, from: std::string::String, to: std::string::String, time: rosrust::Time) -> msg::TransformStamped {
     let transform_stamped_msg = msg::TransformStamped {
-        child_frame_id: to.clone(),
+        child_frame_id: to,
         header: msg::Header {
-            frame_id: from.clone(),
+            frame_id: from,
             stamp: time,
             seq: 0
         },
-        transform: msg::Transform{
-            rotation: msg::Quaternion{
-                x: transform.orientation.x, y:transform.orientation.y, z: transform.orientation.z, w: transform.orientation.w
-            },
-            translation: msg::Vector3{
-                x: transform.position.x, y: transform.position.y, z: transform.position.z
-            }
-        }
+        transform: transform
     };
     transform_stamped_msg
 }
