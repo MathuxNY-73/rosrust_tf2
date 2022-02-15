a4b03db56ec46ea816af4d911949422f27cd6f01
Antoine Pouillaude <a.pouillaude@nomagic.ai>
2022-02-15 13:18:56 +0100

    Removes dependency to rosrust-msg

--- src/msg.rs
+++ src/msg.rs
@@ -1,18 +1,4 @@
-use rosrust_msg as msgs;
-
 use std::cmp::Ordering;
-
-
-pub mod ros_msg {
-    pub use super::msgs::{
-        geometry_msgs::TransformStamped,
-        geometry_msgs::Transform,
-        geometry_msgs::Vector3,
-        geometry_msgs::Quaternion,
-        tf2_msgs::TFMessage,
-        std_msgs::Header
-    };
-}
 
 
 pub trait MessageConverter<Output=Self> {
@@ -30,14 +16,6 @@ pub struct TransformStamped {
     pub header: Header,
     pub child_frame_id: String,
     pub transform: Transform
-}
-
-impl MessageConverter for TransformStamped {
-    type MessageType = ros_msg::TransformStamped;
-
-    fn from_msg(_msg: Self::MessageType) -> TransformStamped { todo!() }
-
-    fn to_msg(_output: TransformStamped) -> Self::MessageType { todo!() }
 }
 
 impl Eq for TransformStamped {}
@@ -60,14 +38,6 @@ pub struct Transform {
     pub rotation: Quaternion
 }
 
-impl MessageConverter for Transform {
-    type MessageType = ros_msg::Transform;
-
-    fn from_msg(_msg: Self::MessageType) -> Transform { todo!() }
-
-    fn to_msg(_output: Transform) -> Self::MessageType { todo!() }
-}
-
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Header {
     pub seq: u32,
@@ -75,27 +45,11 @@ pub struct Header {
     pub stamp: rosrust::Time
 }
 
-impl MessageConverter for Header {
-    type MessageType = ros_msg::Header;
-
-    fn from_msg(_msg: Self::MessageType) -> Header { todo!() }
-
-    fn to_msg(_output: Header) -> Self::MessageType { todo!() }
-}
-
 #[derive(Debug, Clone, PartialEq, PartialOrd)]
 pub struct Vector3 {
     pub x: f64,
     pub y: f64,
     pub z: f64
-}
-
-impl MessageConverter for Vector3 {
-    type MessageType = ros_msg::Vector3;
-
-    fn from_msg(_msg: Self::MessageType) -> Vector3 { todo!() }
-
-    fn to_msg(_output: Vector3) -> Self::MessageType { todo!() }
 }
 
 #[derive(Debug, Clone, PartialEq, PartialOrd)]
@@ -106,23 +60,7 @@ pub struct Quaternion {
     pub w: f64
 }
 
-impl MessageConverter for Quaternion {
-    type MessageType = ros_msg::Quaternion;
-
-    fn from_msg(_msg: Self::MessageType) -> Quaternion { todo!() }
-
-    fn to_msg(_output: Quaternion) -> Self::MessageType { todo!() }
-}
-
 #[derive(Debug, Clone, PartialEq)]
 pub struct TFMessage {
     pub transforms: Vec<TransformStamped>,
-}
-
-impl MessageConverter for TFMessage {
-    type MessageType = ros_msg::TFMessage;
-
-    fn from_msg(_msg: Self::MessageType) -> TFMessage { todo!() }
-
-    fn to_msg(_output: TFMessage) -> Self::MessageType { todo!() }
 }
